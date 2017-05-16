37746fe22dd018dc8ed5321b41cbe4f7a9613e8f
home-pc\Aleksey <atretyakov@akolchin.com>
2017-05-16 18:01:33 +0300

    Add rooms

---
+++ src/main/java/agency/akcom/ggs/server/guice/GetUserListAtRoomHandler.java
@@ -0,0 +1,42 @@
+package agency.akcom.ggs.server.guice;
+
+import java.util.List;
+
+import com.gwtplatform.dispatch.rpc.server.ExecutionContext;
+import com.gwtplatform.dispatch.rpc.server.actionhandler.ActionHandler;
+import com.gwtplatform.dispatch.shared.ActionException;
+
+import agency.akcom.ggs.server.ChatServer;
+import agency.akcom.ggs.shared.action.GetUserListAtRoomAction;
+import agency.akcom.ggs.shared.action.GetUserListAtRoomResult;
+
+public class GetUserListAtRoomHandler 
+		implements ActionHandler<GetUserListAtRoomAction, GetUserListAtRoomResult>{
+
+	@Override
+	public GetUserListAtRoomResult execute(GetUserListAtRoomAction action, ExecutionContext arg1) throws ActionException {
+		ChatServer server = ChatServer.getInstance();
+		
+		/*
+		 * Для теста добавлю 2 пользователя в комнату 
+		 */
+		server.addUserInRoom(0, "Valera");
+		server.addUserInRoom(0, "Tolyan");
+		
+		List<String> users = server.getUsersInRoom(action.getRoom());
+		return new GetUserListAtRoomResult(users);
+	}
+
+	@Override
+	public Class<GetUserListAtRoomAction> getActionType() {
+		return GetUserListAtRoomAction.class;
+	}
+
+	@Override
+	public void undo(GetUserListAtRoomAction arg0, GetUserListAtRoomResult arg1, ExecutionContext arg2)
+			throws ActionException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
